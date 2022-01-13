baa3ad300ab4ee3fc3909dad7cea48b5de673fa4
Wilson Junior <wilsonraj@unipam.edu.br>
2022-01-12 21:02:17 -0300

    Criação da interface do aplicativo

---
+++ atm_consultoria/lib/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  void _abrirEmpresa(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("ATM Consultoria"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_servico.png"),
+                  ),
+                ]
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_contato.png"),
+                  ),
+                ]
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
